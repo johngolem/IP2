9555215dba67a3eafca03a8f70b795f45d73e817
john paul <jpaulopiy@gmail.com>
2021-06-13 10:12:48 +0300

    change days to numbers with 1 repping sunday

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-var akanName=d
+var akanName= "d"
 var gender = prompt ("What is you gender? Male or Female?")
-var dd= prompt("Enter the day date of birth xx");
-var mm= prompt ("Enter month digits of the year you were born xx");
-var cc= prompt("Enter century digits of the year you were born xx98");
-var yy= prompt ("Enter year digits of the year you were born");
+var DD= prompt("Enter the day date of birth xx");
+var MM= prompt ("Enter month digits of the year you were born xx");
+var CC= prompt("Enter century digits of the year you were born xx98");
+var YY= prompt ("Enter year digits of the year you were born 19xx");
 
 function akanName(){
     return ((d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7)
 }
-    if (gender === male && d === monday){ 
+    if (gender === male && d === 2){ 
         console.log ("kwadwo")
-    } else if (gender === male && d === tuesday ){
+    } else if (gender === male && d === 3 ){
         console.log ("kwabena")
-    }  else if (gender === male && d === wednesday ){
+    }  else if (gender === male && d === 4 ){
         console.log ("kwaku")
-    } else if (gender === male && d ===thursday ){
+    } else if (gender === male && d ===5 ){
         console.log ("yaw")
-    } else if (gender === male && d === friday){
+    } else if (gender === male && d === 6){
         console.log ("kofi")
-    } else if (gender === male && d === saturday){
+    } else if (gender === male && d === 7){
         console.log ("kwame")
-    } else (gender === male && d === sunday ){
+    } else (gender === male && d === 1 ){
         console.log ("kwasi")
     } 
-if (gender === female && d === monday){ 
+if (gender === female && d === 2){ 
     console.log ("adwoa")
-} else if (gender === female && d === tuesday ){
+} else if (gender === female && d === 3 ){
     console.log ("abenaa")
-}  else if (gender === female && d === wednesday ){
+}  else if (gender === female && d === 4 ){
     console.log ("akua")
-} else if (gender === female && d ===thursday ){
+} else if (gender === female && d ===5 ){
     console.log ("yaa")
-} else if (gender === female && d === friday){
+} else if (gender === female && d === 6){
     console.log ("afua")
-} else if (gender === female && d === saturday){
+} else if (gender === female && d === 7){
     console.log ("ama")
-} else (gender === female && d === sunday ){
+} else (gender === female && d === 8 ){
     console.log ("akosua")
 } 
