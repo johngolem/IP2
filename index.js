b7156c98317def771b91f423f4d11887a5651a1e
john paul <jpaulopiy@gmail.com>
2021-06-13 19:45:45 +0300

    switch name of akanName to DOB, make it easier to type

--- index.js
+++ index.js
@@ -1,55 +1,44 @@
 
 
-
-function akanName(DD,MM,CC,YY){
-    var gender = prompt("What is your gender? Male or Female?");
+   var gender = prompt("What is your gender? Male or Female?");
+function DOB(CC,YY,MM,DD){
+ 
     var DD= parseInt (prompt ("Enter the day date of birth xx"));
-    var MM= parseInt (prompt ("Enter month digits of the year you were born xx"));
+    var MM= parseInt (prompt ("Enter month digits of when you were born xx"));
     var CC= parseInt (prompt ("Enter century digits of the year you were born xx98"));
     var YY= parseInt (prompt ("Enter year digits of the year you were born 19xx"));
 
     return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % (7);
-   
-
-
 }
 
-
-akanName ();
-
-
-
-
-
-
-
-    if (gender === male && d === 2){ 
-        console.log ("kwadwo");
-    } else if (gender === male && d === 3 ){
-        console.log ("kwabena");
-    }  else if (gender === male && d === 4 ){
-        console.log ("kwaku");
-    } else if (gender === male && d ===5 ){
-        console.log ("yaw");
-    } else if (gender === male && d === 6){
-        console.log ("kofi");
-    } else if (gender === male && d === 7){
-        console.log ("kwame");
-    } else (gender === male && d === 1 );{
+     if (gender === male && DOB <=0) {
         console.log ("kwasi");
+    } else if (gender === male && DOB <=1){ 
+        alert ("kwadwo");
+    } else if (gender === male && DOB === 3 ){
+        alert ("kwabena");
+    }  else if (gender === male && DOB=== 4 ){
+        console.log ("kwaku");
+    } else if (gender === male && DOB===5 ){
+        console.log ("yaw");
+    } else if (gender === male && DOB=== 6){
+        console.log ("kofi");
+    } else (gender === male && DOB=== 7){
+        console.log ("kwame");
+
     } 
-if (gender === female && d === 2){ 
+if (gender === female && DOB === 2){ 
     console.log ("adwoa");
-} else if (gender === female && d === 3 ){
+} else if (gender === female && DOB=== 3 ){
     console.log ("abenaa");
-}  else if (gender === female && d === 4 ){
+}  else if (gender === female && DOB === 4 ){
     console.log ("akua");
-} else if (gender === female && d ===5 ){
+} else if (gender === female && DOB===5 ){
     console.log ("yaa");
-} else if (gender === female && d === 6){
+} else if (gender === female && DOB === 6){
     console.log ("afua");
-} else if (gender === female && d === 7){
+} else if (gender === female && DOB === 7){
     console.log ("ama");
-} else (gender === female && d === 8 );{
+} else (gender === female && DOB === 1 );{
     console.log ("akosua");
 } 
