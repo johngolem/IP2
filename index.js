670d941c1a78f51127e3c0626b8cf084f248d600
john paul <jpaulopiy@gmail.com>
2021-06-13 09:55:05 +0300

    add female conditions

--- index.js
+++ index.js
@@ -10,17 +10,32 @@ function akanName(){
     
     }
     if (gender === male && d === monday){ 
-        console.log ("")
+        console.log ("kwadwo")
     } else if (gender === male && d === tuesday ){
-        console.log ("")
+        console.log ("kwabena")
     }  else if (gender === male && d === wednesday ){
-        console.log ("")
+        console.log ("kwaku")
     } else if (gender === male && d ===thursday ){
-        console.log ("")
+        console.log ("yaw")
     } else if (gender === male && d === friday){
-        console.log ("")
+        console.log ("kofi")
     } else if (gender === male && d === saturday){
-        console.log ("")
+        console.log ("kwame")
     } else (gender === male && d === sunday ){
-        console.log ("")
+        console.log ("kwasi")
     } 
+if (gender === female && d === monday){ 
+    console.log ("adwoa")
+} else if (gender === female && d === tuesday ){
+    console.log ("abenaa")
+}  else if (gender === female && d === wednesday ){
+    console.log ("akua")
+} else if (gender === female && d ===thursday ){
+    console.log ("yaa")
+} else if (gender === female && d === friday){
+    console.log ("afua")
+} else if (gender === female && d === saturday){
+    console.log ("ama")
+} else (gender === female && d === sunday ){
+    console.log ("akosua")
+} 
