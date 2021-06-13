0a2d30efcfcee9f7b1bf9550c9136d26a391ebaf
john paul <jpaulopiy@gmail.com>
2021-06-13 09:31:36 +0300

    add if statement for male users

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function akanName(){
     return ((d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7)
     
-    
+    var gender = prompt ("What is you gender? Male or Female?")
     var dd= prompt("Enter the day date of birth xx");
     var mm= prompt ("Enter month digits of the year you were born xx");
     var cc= prompt("Enter century digits of the year you were born xx98");
@@ -9,3 +9,18 @@ function akanName(){
     
     
     }
+    if (gender === male && d === monday){ 
+        console.log ("")
+    } else if (gender === male && d === tuesday ){
+        console.log ("")
+    }  else if (gender === male && d === wednesday ){
+        console.log ("")
+    } else if (gender === male && d ===thursday ){
+        console.log ("")
+    } else if (gender === male && d === friday){
+        console.log ("")
+    } else if (gender === male && d === saturday){
+        console.log ("")
+    } else (gender === male && d === sunday ){
+        console.log ("")
+    } 
