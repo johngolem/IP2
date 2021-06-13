5b866e24bb9846eceffb02b3514fbfa7831b1533
john paul <jpaulopiy@gmail.com>
2021-06-13 09:20:39 +0300

    created files for IP week 2

---
+++ index.js
@@ -0,0 +1,11 @@
+function akanName(){
+    return ((d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7)
+    
+    
+    var dd= prompt("Enter the day date of birth xx");
+    var mm= prompt ("Enter month digits of the year you were born xx");
+    var cc= prompt("Enter century digits of the year you were born xx98");
+    var yy= prompt ("Enter year digits of the year you were born");
+    
+    
+    }
